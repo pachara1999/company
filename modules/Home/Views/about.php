4866a4dc3aa6f3b54ba972257cfea14d8f5a3ef0
Pachara Bright <43342676+pachara1999@users.noreply.github.com>
2023-01-30 21:36:58 +0700

    Change to templates

--- modules/Home/Views/about.php
+++ modules/Home/Views/about.php
@@ -1,51 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php $this->extend('template/home_layout') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</head>
+<?php $this->section('content') ?>
 
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary py-2">
-        <div class="container ">
-            <div class="collapse navbar-collapse text-center " id="navbarSupportedContent">
-                <img src="https://images.squarespace-cdn.com/content/v1/568d36dfa12f449ad80575ea/1452196668201-5EIWFKPJRM1JG0NEHJ3S/HQC+logo+clear.png?format=1500w" alt="" width="170px" height="80px">
-                <ul class="navbar-nav mx-5">
-                    <li class="nav-item px-2 fw-bold">
-                        <a class="nav-link " aria-current="page" href="<?php echo base_url('/') ?>">Home</a>
-                    </li>
-                    <li class="nav-item px-2 fw-bold">
-                        <a class="nav-link " aria-current="page" href="<?php echo base_url('/about') ?>">ABOUT US</a>
-                    </li>
-                    <li class="nav-item px-2 fw-bold">
-                        <a class="nav-link" href="<?php echo base_url('/team') ?>">TEAM</a>
-                    </li>
-                    <li class="nav-item px-2 fw-bold ">
-                        <a class="nav-link" href="#">PORTFOLIO</a>
-                    </li>
-                    <li class="nav-item px-2 fw-bold">
-                        <a class="nav-link" href="#">NEWS & INSIGHTS</a>
-                    </li>
-                    <li class="nav-item px-2 fw-bold">
-                        <a class="nav-link" href="#">CONTACT</a>
-                    </li>
-                    <li class="nav-item px-2 fw-bold">
-                        <a class="nav-link" href="#">PROJECT PULSECHECK</a>
-                    </li>
-                    <li class="nav-item px-2 fw-bold">
-                        <a class="nav-link" href="#">LP PORTAL</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</body>
+<?php $this->endSection() ?>
 
-</html>
+<?php $this->section('scripts') ?>
+
+<?php $this->endSection() ?>
